8d6f563fe519fc6e0bbfe5616db402bf748f815c
Sma-source <rabhi.smain@gmail.com>
2024-03-20 17:57:47 +0100

    create logout function

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,8 +3,17 @@ import React from "react";
 import { ModeToggle } from "../ModeToggle";
 import { Button } from "../ui/button";
 import LogIn from "./LogIn";
+import getUserSession from "@/lib/getUserSession";
+import createClient from "@/lib/supabase/server";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { data } = await getUserSession();
+
+  const logoutAction = async () => {
+    "use server";
+    const supabase = await createClient();
+    await supabase.auth.signOut();
+  };
   return (
     <nav className="border-b bg-background h-[10vh] flex items-center">
       <div className="container flex items-center justify-between">
@@ -15,9 +24,18 @@ const Navbar = () => {
         </Link>
         <div className="flex items-center gap-x-5">
           <ModeToggle />
-          <div className="flex items-center gap-x-5">
-            <LogIn />
-          </div>
+          {!data.session && (
+            <div className="flex items-center gap-x-5">
+              <LogIn />
+            </div>
+          )}
+          {data.session && (
+            <form action={logoutAction} className="flex items-center">
+              <div className="gap-x-5">
+                <Button>Logout</Button>
+              </div>
+            </form>
+          )}
         </div>
       </div>
     </nav>
