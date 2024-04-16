0b1fec68fac940fc4f97dabf1b649b76facb4383
Sma-source <rabhi.smain@gmail.com>
2024-04-16 17:14:03 +0200

    create page component to render and send form value

--- app/dashboard/acj/create/page.tsx
+++ app/dashboard/acj/create/page.tsx
@@ -1,7 +1,44 @@
+"use client";
 import React from "react";
+import AcjForm from "../components/AcjForm";
+import { PostgrestSingleResponse } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
+import { toast } from "@/components/ui/use-toast";
+import { AcjFormSchemaType } from "../../schema";
+import { createBlog } from "@/lib/actions/acj";
 
 const CreateNew = () => {
-  return <div>CreateNew</div>;
+  const router = useRouter();
+  const handleCreate = async (data: AcjFormSchemaType) => {
+    const result = JSON.parse(await createBlog(data));
+
+    const { error } = result as PostgrestSingleResponse<null>;
+    if (error?.message) {
+      toast({
+        title: "Fail to create a post 😢",
+        description: (
+          <pre className="mt-2 w-[340px] rounded-md p-4">
+            <code>{error.message}</code>
+          </pre>
+        ),
+      });
+    } else {
+      toast({
+        title: "Successfully create a post 🎉",
+        description: data.title,
+      });
+      router.push("/dashboard");
+    }
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md  p-4">
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  };
+  return <AcjForm onHandleSubmit={handleCreate} />;
 };
 
 export default CreateNew;
