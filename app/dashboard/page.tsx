23f7671e7402158dddd9b6908a4a81dd8e26c428
Sma-source <rabhi.smain@gmail.com>
2024-04-16 17:11:52 +0200

    display data to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,29 +1,41 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import Link from "next/link";
-import React from "react";
+import { readBlog } from "@/lib/actions/acj";
 
-const Dashboard = () => {
+import Link from "next/link";
+
+const Dashboard = async () => {
+  const { data: blogs } = await readBlog();
+  // const user = await getUserSession();
+  // console.log(user.data.session?.user.id);
+
   return (
     <div className="grid items-start gap-y-8">
       <div className="flex items-center justify-between px-2">
         <div className="grid gap-1">
           <h1 className="text-3xl md:text-4xl">Your Notes</h1>
           <p className="text-lg text-muted-foreground">
-            Here you can see and create new notes
+            Here you can see and create new notes for user id
           </p>
         </div>
         <Button>
           <Link href="/dashboard/acj/create">Create a new Note</Link>
         </Button>
       </div>
+
       <div className="flex flex-col gap-y-4">
-        <Card className="flex items-center justify-between p-4">
-          <div>
-            <h2 className="font-semibold text-xl text-primary">Nouvelle acj</h2>
-            <p>12 février 2018</p>
-          </div>
-        </Card>
+        {blogs?.map((blog, index) => {
+          return (
+            <Card key={index} className="flex items-center justify-between p-4">
+              <div>
+                <h2 className="font-semibold text-xl text-primary">
+                  {blog.title}
+                </h2>
+                <p>{blog.content}</p>
+              </div>
+            </Card>
+          );
+        })}
       </div>
     </div>
   );
