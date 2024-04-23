b4aa01abdd12c344bcc7f132636acfbeb598166d
Sma-source <rabhi.smain@gmail.com>
2024-04-23 16:41:16 +0200

    add placeholder ui for loading page

--- app/(home)/(default)/loading.tsx
+++ app/(home)/(default)/loading.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const loading = () => {
-  return <div>loading</div>;
+  return (
+    <div className="w-full grid md:grid lg:grid-cols-2 xl:grid-cols-3 gap-5 p-5 xl:p-0">
+      {[1, 2, 3, 4, 5]?.map((_, index) => {
+        return (
+          <div>
+            <Skeleton className="w-full h-20 sm:w-full  md:h-18 xl:h-24 rounded-lg" />
+            <div className="space-y-2">
+              {/* <Skeleton className="h-4 w-[250px]" /> */}
+              <Skeleton className="mt-2 h-4 w-[200px]" />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default loading;
