cce627d6ede40c3f0e68299167bfd6ffbfa845db
Sma-source <rabhi.smain@gmail.com>
2024-04-24 19:29:07 +0200

    add div to ix warning

--- app/(home)/(default)/page.tsx
+++ app/(home)/(default)/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { readBlog } from "@/lib/actions/acj";
 import { StarIcon } from "lucide-react";
+import Link from "next/link";
 
 export default async function Home() {
   const { data: blogs } = await readBlog();
@@ -36,35 +37,34 @@ export default async function Home() {
         <div className="items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">
           {blogs?.map((blog, index) => {
             return (
-              <div
-                key={index}
-                className="flex items-center justify-center [&>div]:w-full"
-              >
-                <div className="rounded-xl border bg-card text-card-foreground mt-5">
-                  <div className="flex-col p-6 grid grid-cols-1 items-start gap-4 space-y-0">
-                    <div className="space-y-2">
-                      <h3 className="font-semibold text-lg leading-none tracking-tight text-primary">
-                        {blog.title}
-                      </h3>
-                      <p className="text-sm text-muted-foreground">
-                        {blog.content}
-                      </p>
+              <div key={index}>
+                <Link href={"/acj/" + blog.id}>
+                  <div className="rounded-xl border bg-card text-card-foreground mt-5">
+                    <div className="flex-col p-6 grid grid-cols-1 items-start gap-4 space-y-0">
+                      <div className="space-y-2">
+                        <h3 className="font-semibold text-lg leading-none tracking-tight text-primary">
+                          {blog.title}
+                        </h3>
+                        <p className="text-sm text-muted-foreground">
+                          {blog.content}
+                        </p>
+                      </div>
+                      <div className="flex items-center space-x-1 rounded-md bg-secondary text-secondary-foreground">
+                        <button className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground hover:bg-secondary/80 h-9 py-2 px-3 shadow-none">
+                          <StarIcon className="mr-2 h-4 w-4" />
+                          Favoris
+                        </button>
+                      </div>
                     </div>
-                    <div className="flex items-center space-x-1 rounded-md bg-secondary text-secondary-foreground">
-                      <button className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground hover:bg-secondary/80 h-9 py-2 px-3 shadow-none">
-                        <StarIcon className="mr-2 h-4 w-4" />
-                        Favoris
-                      </button>
+                    <div className="p-6 pt-0">
+                      <div className="flex space-x-4 text-sm text-muted-foreground">
+                        <div className="flex items-center">Actualité</div>
+                        <div className="flex items-center">20 avril 2023</div>
+                        <div>test</div>
+                      </div>
                     </div>
                   </div>
-                  <div className="p-6 pt-0">
-                    <div className="flex space-x-4 text-sm text-muted-foreground">
-                      <div className="flex items-center">Actualité</div>
-                      <div className="flex items-center">20 avril 2023</div>
-                      <div>test</div>
-                    </div>
-                  </div>
-                </div>
+                </Link>
               </div>
             );
           })}
