81b09bc17c32affb73ffad34ded99949ddd13d4d
Sma-source <rabhi.smain@gmail.com>
2024-04-26 17:29:41 +0200

    create function to handle update content on submit

--- app/dashboard/acj/edit/[id]/components/EditForm.tsx
+++ app/dashboard/acj/edit/[id]/components/EditForm.tsx
@@ -3,9 +3,32 @@ import { AcjFormSchemaType } from "@/app/dashboard/schema";
 import React from "react";
 import AcjForm from "../../../components/AcjForm";
 import { IAcj } from "@/lib/types";
+import { updateBlogById } from "@/lib/actions/acj";
+import { toast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 const EditForm = ({ acj }: { acj: IAcj }) => {
-  return <AcjForm onHandleSubmit={() => {}} acj={acj} />;
+  const router = useRouter();
+  const onHandleSubmit = async (data: AcjFormSchemaType) => {
+    const result = JSON.parse(await updateBlogById(acj.id, data));
+
+    if (result.error) {
+      toast({
+        title: "Fail to update ",
+        description: (
+          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+            <code className="text-white">{result.error?.message}</code>
+          </pre>
+        ),
+      });
+    } else {
+      toast({
+        title: "Successfully update 🎉",
+      });
+      router.push("/dashboard");
+    }
+  };
+  return <AcjForm onHandleSubmit={onHandleSubmit} acj={acj} />;
 };
 
 export default EditForm;
