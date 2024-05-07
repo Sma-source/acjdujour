8c3c891187f1934ff6fcce40b1c45f3891bab043
Sma-source <rabhi.smain@gmail.com>
2024-05-07 15:45:41 +0200

    update: color text when dark mode on

--- app/acj/[id]/page.tsx
+++ app/acj/[id]/page.tsx
@@ -15,7 +15,7 @@ const page = async ({ params }: { params: { id: string } }) => {
 
   return (
     <div className="max-w-5xl mx-auto min-h-screen  pt-10 space-y-10">
-      <div className="sm:px-10 space-y-5">
+      <div className="sm:px-10 px-5 space-y-5">
         <h1 className=" text-3xl font-bold dark:text-gray-200">
           {blog?.title}
         </h1>
@@ -25,7 +25,9 @@ const page = async ({ params }: { params: { id: string } }) => {
           }).format(when)}
         </p>
       </div>
-      <div className="sm:px-10 space-y-5 text-gray-800">{blog?.content}</div>
+      <div className="sm:px-10 px-5 space-y-5 text-gray-800 dark:text-gray-100">
+        {blog?.content}
+      </div>
     </div>
   );
 };
