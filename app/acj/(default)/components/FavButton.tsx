dec246ad2893659d3fbecc117581f1855d24d8a5
Sma-source <rabhi.smain@gmail.com>
2024-05-16 21:01:24 +0200

    create favButton component and add form to handle add and delete favorite

---
+++ app/acj/(default)/components/FavButton.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { addFavorite, deleteFavorite } from "@/lib/actions/acj";
+import { Heart } from "lucide-react";
+import React from "react";
+
+const FavButton = ({
+  userId,
+  acjId,
+  isFavorited,
+}: {
+  userId: string;
+  acjId: string;
+  isFavorited: boolean;
+}) => {
+  return (
+    <div className="flex items-center">
+      {isFavorited ? (
+        <form action={deleteFavorite}>
+          <input type="hidden" name="acjId" value={acjId} />
+          <input type="hidden" name="userId" value={userId} />
+          <button
+            type="submit"
+            className="bg-transparent border-none text-red-500 cursor-pointer hover:text-red-500 hover:scale-110 transition duration-300"
+          >
+            <Heart className="w-6 h-6 fill-red-500" />
+          </button>
+        </form>
+      ) : (
+        <form action={addFavorite}>
+          <input type="hidden" name="acjId" value={acjId} />
+          <input type="hidden" name="userId" value={userId} />
+          <button
+            type="submit"
+            className="bg-transparent border-none text-red-500 cursor-pointer hover:text-red-500 hover:scale-110 transition duration-300"
+          >
+            <Heart className="w-6 h-6 fill-none" />
+          </button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default FavButton;
