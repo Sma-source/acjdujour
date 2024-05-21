ce44ac429debaaa2f980cd8a2be161d66d925e4d
Sma-source <rabhi.smain@gmail.com>
2024-05-21 23:10:20 +0200

    display favs

--- app/dashboard/favoris/page.tsx
+++ app/dashboard/favoris/page.tsx
@@ -1,7 +1,36 @@
+import { Card } from "@/components/ui/card";
+import { readFavs } from "@/lib/actions/acj";
+import getUserSession from "@/lib/getUserSession";
+import Link from "next/link";
 import React from "react";
 
-const Favoris = () => {
-  return <div>Favoris</div>;
+const Favoris = async () => {
+  const {
+    data: { user },
+  } = await getUserSession();
+
+  const idUser = user?.id;
+
+  const { data: favs } = await readFavs(idUser || "");
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      {favs?.map((fav) => (
+        <Card
+          key={fav.acj_id}
+          className="flex items-center justify-between p-4"
+        >
+          <Link href={"/acj/" + fav.acj.id}>
+            <div>
+              <h2 className="font-semibold text-xl text-primary">
+                {fav.acj.title}
+              </h2>
+            </div>
+          </Link>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default Favoris;
