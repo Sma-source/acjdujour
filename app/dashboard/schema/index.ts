36240385d6341745a6fa9d0490e89e5bbf2bc05f
Sma-source <rabhi.smain@gmail.com>
2024-07-15 18:35:13 +0200

    add category to form schema

--- app/dashboard/schema/index.ts
+++ app/dashboard/schema/index.ts
@@ -7,6 +7,9 @@ export const AcjFormSchema = z.object({
   content: z.string().min(20, {
     message: "Content is too short",
   }),
+  category_id: z.string({
+    required_error: "Please select a categorie to display.",
+  }),
 });
 
 export type AcjFormSchemaType = z.infer<typeof AcjFormSchema>;
