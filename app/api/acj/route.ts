b37681504fe72e6f9b03c3e567f709ab684aeda6
Sma-source <rabhi.smain@gmail.com>
2024-07-15 18:33:21 +0200

    add new route for category-id

--- app/api/acj/route.ts
+++ app/api/acj/route.ts
@@ -10,12 +10,24 @@ export const GET = async (request: Request) => {
   const { searchParams } = new URL(request.url);
 
   const id = searchParams.get("id");
+  const category_id = searchParams.get("category_id");
 
   if (id === "*") {
     const { data, error } = await supabase.from("acj").select("id");
     if (error) {
       return new Response(JSON.stringify({ error }), { status: 500 });
     }
+    return new Response(JSON.stringify({ data }), { status: 200 });
+  } else if (category_id) {
+    const { data, error } = await supabase
+      .from("acj")
+      .select("*")
+      .eq("category_id", category_id);
+
+    if (error) {
+      return new Response(JSON.stringify({ error }), { status: 500 });
+    }
+
     return new Response(JSON.stringify({ data }), { status: 200 });
   } else if (id) {
     const { data, error } = await supabase
